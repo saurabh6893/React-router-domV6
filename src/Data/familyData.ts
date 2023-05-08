98cc9b86a4554f388eb85d98ec39d6367e8ff9ee
Saurabh Kedar <saurabhkedar@gmail.com>
2023-05-08 19:27:35 +0530

    made neccasary Ui changes and added temp data

---
+++ src/Data/familyData.ts
@@ -0,0 +1,102 @@
+export const families = [
+  {
+    "familyName": "Smith",
+    "email": "smith@example.com",
+    "flatNo": "101",
+    "vehicleNo": "AB1234",
+    "primary": "555-1234",
+    "secondary": "555-5678",
+    "password": "password123",
+
+  },
+  {
+    "familyName": "Jones",
+    "email": "jones@example.com",
+    "flatNo": "207",
+    "vehicleNo": "",
+    "primary": "555-9876",
+    "secondary": "",
+    "password": "secret789",
+
+  },
+  {
+    "familyName": "Williams",
+    "email": "williams@example.com",
+    "flatNo": "310",
+    "vehicleNo": "CD5678",
+    "primary": "555-5555",
+    "secondary": "",
+    "password": "password456",
+
+  },
+  {
+    "familyName": "Brown",
+    "email": "brown@example.com",
+    "flatNo": "401",
+    "vehicleNo": "EF9012",
+    "primary": "555-2222",
+    "secondary": "",
+    "password": "abc123",
+
+  },
+  {
+    "familyName": "Wilson",
+    "email": "wilson@example.com",
+    "flatNo": "502",
+    "vehicleNo": "",
+    "primary": "555-3333",
+    "secondary": "",
+    "password": "def456",
+
+  },
+  {
+    "familyName": "Taylor",
+    "email": "taylor@example.com",
+    "flatNo": "611",
+    "vehicleNo": "GH3456",
+    "primary": "555-4444",
+    "secondary": "",
+    "password": "ghi789",
+
+  },
+  {
+    "familyName": "Davis",
+    "email": "davis@example.com",
+    "flatNo": "709",
+    "vehicleNo": "",
+    "primary": "555-8888",
+    "secondary": "",
+    "password": "jkl012",
+
+  },
+  {
+    "familyName": "Miller",
+    "email": "miller@example.com",
+    "flatNo": "812",
+    "vehicleNo": "",
+    "primary": "555-9999",
+    "secondary": "",
+    "password": "mno345",
+
+  },
+  {
+    "familyName": "Wilson",
+    "email": "wilson@example.com",
+    "flatNo": "901",
+    "vehicleNo": "IJ6789",
+    "primary": "555-7777",
+    "secondary": "",
+    "password": "pqr678",
+
+  },
+  {
+    "familyName": "Clark",
+    "email": "clark@example.com",
+    "flatNo": "1006",
+    "vehicleNo": "",
+    "primary": "555-6666",
+    "secondary": "",
+    "password": "stu901",
+
+  }
+]
