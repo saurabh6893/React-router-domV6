9edb51b505c46a2b6cf8d85e78d48b2c1a3ad56c
Saurabh Kedar <saurabhkedar@gmail.com>
2023-05-09 16:50:26 +0530

    added global search

--- src/Pages/Search/SearchPage.tsx
+++ src/Pages/Search/SearchPage.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { families } from '../../Data/familyData'
 import { useTable } from 'react-table'
 import './SearchPage.css'
+
 const SearchPage = () => {
   const data = useMemo(() => families, [])
   const columns: any = useMemo(
@@ -16,14 +17,39 @@ const SearchPage = () => {
     []
   )
 
+  const [searchValue, setSearchValue] = useState('')
+
+  const filteredData = useMemo(() => {
+    if (!searchValue) {
+      return data
+    }
+    return data.filter((family) => {
+      return Object.values(family).some((value) =>
+        value.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    })
+  }, [data, searchValue])
+
   const { getTableBodyProps, getTableProps, headerGroups, rows, prepareRow } =
     useTable({
       columns,
-      data,
+      data: filteredData,
     })
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value)
+  }
 
   return (
     <div className='tablebody'>
+      <div className='searchbar'>
+        <input
+          type='text'
+          placeholder='Search...'
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
+      </div>
       <div className='container'>
         <table {...getTableProps()}>
           <thead>
@@ -56,5 +82,3 @@ const SearchPage = () => {
 }
 
 export default SearchPage
-
-// const data = families.map((f: any) => <h1>{f.email}</h1>)
